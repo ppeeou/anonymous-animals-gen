51c39bd70a4f0e49245547165041def4cd6f574a
Hyunwoo.jo <hyunwoo.jo@navercorp.com>
2021-02-06 22:02:03 +0900

    Add anonymous generate logic

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import adjectives from "./adjectives";
+import animals from "./animals";
+import * as animalImages from "./animalImages";
+
+const animalSize = animals.length;
+const adjectiveSize = adjectives.length;
+
+/**
+ * @typedef options
+ * @property {string} prefix
+ */
+/**
+ * @typedef info
+ * @property {string} name
+ * @property {string} image
+ * @property {string} animal
+ */
+export default {
+  /**
+   * @param {options} options
+   * @returns {info}
+   */
+  generate(options = {}) {
+    const { prefix } = options;
+    const adjective = adjectives[Math.floor(Math.random() * adjectiveSize)];
+    const animal = animals[Math.floor(Math.random() * animalSize)];
+    const image = animalImages[animal];
+    const name = `${adjective} ${animal}`;
+
+    return {
+      name: prefix ? `${prefix} ${name}` : name,
+      image,
+      animal,
+    };
+  },
+
+  /**
+   * @param {string} animal
+   * @returns {string}
+   */
+  getImage(animal) {
+    const image = animalImages[animal];
+    return image;
+  },
+};
---
+++ webpack.dev.js
@@ -0,0 +1,43 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "anonymous.js",
+    library: {
+      root: "anonymous",
+    },
+    libraryTarget: "umd",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.png$/i,
+        use: {
+          loader: "url-loader",
+          options: {
+            generator: (content, mimetype, encoding) => {
+              return `data:${mimetype}${
+                encoding ? `;${encoding}` : ""
+              },${content.toString(encoding)}`;
+            },
+          },
+        },
+      },
+    ],
+  },
+
+  devtool: "source-map",
+};
