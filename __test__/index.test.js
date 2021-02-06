d48624cdb3a128d230ecf45c11ef57ca54cdf908
Hyunwoo.jo <hyunwoo.jo@navercorp.com>
2021-02-06 22:02:17 +0900

    Add test code

---
+++ __test__/index.test.js
@@ -0,0 +1,29 @@
+import anonymous from "../dist/anonymous";
+import animals from "../src/animals";
+
+describe("anonymous", () => {
+  it("should exist all animal images", () => {
+    animals.forEach((animal) => {
+      const animalImage = anonymous.getImage(animal);
+      expect(typeof animalImage).toBe("string");
+    });
+  });
+
+  it("should exist sampling of animal random", () => {
+    for (let i = 0; i < 20; i++) {
+      const { name, image, animal } = anonymous.generate();
+      expect(animals.indexOf(animal)).not.toBe(-1);
+      expect(typeof name).toBe("string");
+      expect(typeof image).toBe("string");
+    }
+  });
+
+  it("should have prefix when prefix is assigned", () => {
+    const prefix = "Anonymous";
+    const { name, image, animal } = anonymous.generate({ prefix });
+    expect(animals.indexOf(animal)).not.toBe(-1);
+    expect(name.slice(0, prefix.length)).toBe(prefix);
+    expect(typeof name).toBe("string");
+    expect(typeof image).toBe("string");
+  });
+});
